05164d395d5265b61778155d7059bf7c299bef79
taimoor-shan <tshanajk@gmail.com>
2025-05-23 14:00:54 +0500

    Deployed first time to Naseer/Almuqadam

--- vercel.js
+++ vercel.js
@@ -5,6 +5,6 @@
 import 'dotenv/config';
 
 // Provide fallback values for required environment variables
-export const DATABASE_URL = process.env.DATABASE_URL || 'postgres://dummy:dummy@localhost:5432/dummy';
-export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'dummy-password-for-build';
+export const DATABASE_URL = process.env.DATABASE_URL || 'postgres://neondb_owner:npg_qzXhDOWv26TQ@ep-blue-sunset-ab1hs87n-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require';
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'Qwerty@007';
 export const ORIGIN = process.env.ORIGIN || 'https://dummy-origin-for-build.vercel.app';
